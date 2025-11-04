205faa76d4ddc5322f72dac70a9c1ec6e7385398
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-04 22:04:18 +0530

    Firs Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Profile],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('style-using-angular');
+  protected readonly title:string = 'style-using-angular';
 }
